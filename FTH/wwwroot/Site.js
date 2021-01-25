0137e864e557cdf67557a0ab137633214a69bbc2
Chris Jackson <chris.jackson92@gmail.com>
2021-01-25 23:22:22 +0000

    So it appears that you need semi-colons at the end of JS functions otherwise the minifier produces bad code.

--- FTH/wwwroot/Site.js
+++ FTH/wwwroot/Site.js
@@ -1,90 +1,32 @@
 ﻿
 window.blazor = {};
 
-window.blazor.tempDonation=null;
-
-window.blazor.bindPaypalButton = (selector) => {
-
-    paypal.Buttons({
-        style: {
-            layout: 'horizontal'
-        },
-        createOrder: function(data, actions) {
-
-            var donation = DotNet.invokeMethod("FTH", "GetDonation").result;
-            if (donation.total < 1) {
-                return null;
-            }
-
-            window.blazor.tempDonation = donation;
-
-            return actions.order.create({
-                purchase_units: [{
-                    amount: {
-                        value: donation.total
-                    }
-                }]
-            });
-        },
-        onApprove: function(data, actions) {
-            return actions.order.capture().then(function (details) {
-                // This function shows a transaction success message to your buyer.
-                var don = window.blazor.tempDonation;
-
-                try {
-                    DotNet.invokeMethod("FTH",
-                        "CreateDonation",
-                        details.id,
-                        don.name,
-                        don.anon,
-                        don.comment,
-                        don.email);
-                } catch (e) {
-                    //ignore
-                }
-                
-
-            });
-        }
-    }).render(selector);;
-}
-
-
 window.background = () => {
     var t = new Trianglify({
-        x_gradient: ["#E24015", "#87370C", "#730000", "#391100","#000000"],
+        x_gradient: ["#E24015", "#87370C", "#730000", "#391100", "#000000"],
         noiseIntensity: 0,
         cellsize: 100
     });
     var pattern = t.generate(window.innerWidth, window.innerHeight);
     document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
-}
-
+};
 
 window.background();
 
-var exampleCallback = function () {
-    console.log('Order complete!');
-};
-
-
 window.blazor.createEventBriteFrame = (id) => {
-    window.EBWidgets.createWidget({
-        // Required
-        widgetType: 'checkout',
-        eventId: '137912330493',
-        iframeContainerId: id,
 
-        // Optional
-        iframeContainerHeight: 1455,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
-        onOrderComplete: exampleCallback  // Method called when an order has successfully completed
-    });
-}
+    if (window.EBWidgets != null) {
+        window.EBWidgets.createWidget({
+            // Required
+            widgetType: 'checkout',
+            eventId: '137912330493',
+            iframeContainerId: id,
 
-window.blazor.fitText = (selector) => {
-    $(selector).fitText(1.2, { minFontSize: '20px', maxFontSize: '60px' });
-}
-
+            // Optional
+            iframeContainerHeight: 1455, // Widget height in pixels. Defaults to a minimum of 425px if not provided
+        });
+    }
+};
 
 /*global jQuery */
 /*!
@@ -129,3 +71,9 @@ window.blazor.fitText = (selector) => {
     };
 
 })(jQuery);
+
+
+window.blazor.fitText = (selector) => {
+    $(selector).fitText(1.2, { minFontSize: '20px', maxFontSize: '60px' });
+};
+
